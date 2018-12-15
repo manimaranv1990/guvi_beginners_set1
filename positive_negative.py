1c7ad8e7a18099946b4efef517d0bdd3b3b0cb94
manimaranv1990 <45891826+manimaranv1990@users.noreply.github.com>
2018-12-15 17:55:40 +0530

    Add files via upload

---
+++ positive_negative.py
@@ -0,0 +1,10 @@
+nu = float(input("Enter a Number: "))
+
+if nu == 0:
+   print("Entered Number is Zero")
+
+elif nu > 0:
+   print("Entered number is Positive number")
+
+else:
+   print("Entered Number is Negative number")
