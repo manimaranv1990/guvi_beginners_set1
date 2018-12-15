3090459bae78fff79dbe2dbe08c3c5b2bef761ea
manimaranv1990 <45891826+manimaranv1990@users.noreply.github.com>
2018-12-15 19:33:14 +0530

    Add files via upload

---
+++ vowelorconstant.py
@@ -0,0 +1,7 @@
+ch = input("Enter a Character: ")
+
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print("Entered Character", ch, "is a Vowel")
+
+else:
+    print("Entered Character", ch, "is a Consonant")
